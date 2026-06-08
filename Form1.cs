25c62382f630a8a7499922d941c737117ae3ab32
Daria Ozhoh <daraozog@mail.com>
2026-06-08 11:14:47 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Створити застосунок Windows Forms згідно зі своїм варіантом (відповідно
+//порядковому номеру у журналі). Кожен застосунок повинен складатися з щонайменше двох
+//частин:
+//• Клас(и) бізнес - логіки, який інкапсулює дані та операції і містить щонайменше одну
+//подію.
+//• Клас форми, який містить елементи керування та виступає підписником на події
+//логіки, оновлюючи GUI відповідно до них
+
+//Будильник.Логіка: Клас AlarmClock, який використовує Timer і має метод
+//SetAlarm(time). Коли поточний час збігається із заданим, ініціюється подія Alarm.
+//Інтерфейс: DateTimePicker для встановлення часу, кнопка "Встановити будильник". При
+//спрацюванні події Alarm програма має видавати звуковий сигнал та/або показувати
+//MessageBox.
+
+namespace OP_pract13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OP_pract13;
+//Будильник.Логіка: Клас AlarmClock, який використовує Timer і має метод
+//SetAlarm(time). Коли поточний час збігається із заданим, ініціюється подія Alarm.
+//Інтерфейс: DateTimePicker для встановлення часу, кнопка "Встановити будильник". При
+//спрацюванні події Alarm програма має видавати звуковий сигнал та/або показувати
+//MessageBox.
+public class AlarmClock {
+
+}
+
+
+
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
